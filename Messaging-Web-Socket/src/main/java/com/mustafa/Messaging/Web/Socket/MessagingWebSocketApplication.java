9431c171dff1b15d04347cd793b6792e16168342
mustafaakburak <onlysecretgame@gmail.com>
2021-12-25 23:59:27 +0300

    initialcommit

---
+++ Messaging-Web-Socket/src/main/java/com/mustafa/Messaging/Web/Socket/MessagingWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.mustafa.Messaging.Web.Socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingWebSocketApplication.class, args);
+	}
+
+}
---
+++ Messaging-Web-Socket/src/test/java/com/mustafa/Messaging/Web/Socket/MessagingWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafa.Messaging.Web.Socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
